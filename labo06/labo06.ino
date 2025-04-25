5c9b8eb60244e2a8fb9dda2c3568c8897c8e61cf
cuzimapanda2003 <marcblais03@gmail.com>
2025-04-25 14:14:33 -0400

    Update labo06.ino

--- labo06/labo06.ino
+++ labo06/labo06.ino
@@ -1,6 +1,16 @@
 #include <HCSR04.h>
 #include <LCD_I2C.h>
 #include <U8g2lib.h>
+#include <AccelStepper.h>
+
+#define MOTOR_INTERFACE_TYPE 4
+
+#define IN_1 31
+#define IN_2 33
+#define IN_3 35
+#define IN_4 37
+
+AccelStepper myStepper(MOTOR_INTERFACE_TYPE, IN_1, IN_3, IN_2, IN_4);
 
 
 
@@ -9,13 +19,12 @@
 #define CS_PIN 32  // Chip Select
 
 U8G2_MAX7219_8X8_F_4W_SW_SPI u8g2(
-  U8G2_R0,       // rotation
-  /* clock=*/ CLK_PIN, // pin Arduino reliée à CLK (horloge)
-  /* data=*/ DIN_PIN,  // pin Arduino reliée à DIN (données)
-  /* cs=*/ CS_PIN,    // pin Arduino reliée à CS (chip select)
-  /* dc=*/ U8X8_PIN_NONE,
-  /* reset=*/ U8X8_PIN_NONE
-);
+  U8G2_R0,             // rotation
+  /* clock=*/CLK_PIN,  // pin Arduino reliée à CLK (horloge)
+  /* data=*/DIN_PIN,   // pin Arduino reliée à DIN (données)
+  /* cs=*/CS_PIN,      // pin Arduino reliée à CS (chip select)
+  /* dc=*/U8X8_PIN_NONE,
+  /* reset=*/U8X8_PIN_NONE);
 
 #define TRIGGER_PIN 3
 #define ECHO_PIN 2
@@ -33,6 +42,8 @@ HCSR04 hc(TRIGGER_PIN, ECHO_PIN);
 const long interval = 100;
 
 enum etat_distance { TROP_PRES,
+                     TROP_PROCHE,
+                     MOTEUR,
                      TROP_LOIN };
 etat_distance etatDistance = TROP_LOIN;
 int distance;
@@ -42,6 +53,19 @@ unsigned long tempsDepuisLoin = 0;
 bool objetEstLoin = false;
 const unsigned long delaiExtinction = 3000;
 bool firstTime = true;
+int alerte = 15;
+
+
+long targetPosition = 0;
+long previousTarget = -1;
+float degree;
+
+int maxSpeed = 500;
+int maxAccel = 100;
+int inf = 30;
+int sup = 60;
+
+
 
 
 void lcdstart() {
@@ -63,15 +87,17 @@ void setup() {
   lcd.begin();
   lcd.backlight();
   lcdstart();
+
+  myStepper.setMaxSpeed(maxSpeed);
+  myStepper.setAcceleration(maxAccel);
 }
 
 void loop() {
- // chercherDistance();
- // affichage();
- // etatSystem();
-
-    commande();
-  
+  myStepper.run();
+  chercherDistance();
+  affichage();
+  etatSystem();
+  commande();
 }
 
 
@@ -85,9 +111,15 @@ void chercherDistance() {
 
 void etatSystem() {
   unsigned long currentTimes = millis();
-  if (distance <= 15) {
 
+  if (distance <= alerte) {
     etatDistance = TROP_PRES;
+    objetEstLoin = false;
+  } else if (distance > alerte && distance < inf) {
+    etatDistance = TROP_PROCHE;
+    objetEstLoin = false;
+  } else if (distance >= inf && distance <= sup) {
+    etatDistance = MOTEUR;
     objetEstLoin = false;
   } else {
     etatDistance = TROP_LOIN;
@@ -98,32 +130,43 @@ void etatSystem() {
   }
 
   switch (etatDistance) {
-
     case TROP_PRES:
       tropPres();
       girophare();
       alarme();
+      break;
 
+    case TROP_PROCHE:
+      // tu peux ajouter une fonction si besoin
+      break;
+
+    case MOTEUR:
+      targetPosition = map(distance, 30, 60, 0, 1024);
+      if (targetPosition != previousTarget) {
+        myStepper.moveTo(targetPosition);
+        previousTarget = targetPosition;
+      }
+      affichageMilieu(targetPosition);
       break;
 
     case TROP_LOIN:
       tropLoin();
 
-
       if (currentTimes - tempsDepuisLoin >= delaiExtinction) {
         firstTime = false;
         girophareEteint();
         alarmeOff();
-        girophareEteint();
       } else if (firstTime == false) {
-
         girophare();
         alarme();
       }
-
       break;
   }
 }
+
+
+
+
 
 
 void affichage() {
@@ -142,17 +185,31 @@ void affichage() {
   }
 }
 
-void tropLoin() {
-  lcd.print("obj  : ");
-  lcd.setCursor(7, 1);
-  lcd.print("trop loin");
+void affichageMilieu(long steps) {
+  degree = (steps / 2048.0) * 360.0;
+  lcd.setCursor(0, 1);
+  lcd.print("                ");  
+  lcd.setCursor(0, 1);
+  lcd.print("Deg  : ");
+  lcd.print(degree, 1);
 }
 
-void tropPres() {
-  lcd.print("obj  : ");
-  lcd.setCursor(7, 1);
-  lcd.print("trop pret");
+
+void tropLoin() {
+  lcd.setCursor(0, 1);
+  lcd.print("                ");  
+  lcd.setCursor(0, 1);
+  lcd.print("obj  : trop loin");
 }
+
+
+void tropPres() {
+  lcd.setCursor(0, 1);
+  lcd.print("                ");  
+  lcd.setCursor(0, 1);
+  lcd.print("obj  : trop pret");
+}
+
 
 
 void alarme() {
@@ -187,15 +244,15 @@ void girophareEteint() {
 }
 
 
-void ecranSetup(){
+void ecranSetup() {
   u8g2.begin();
   u8g2.setContrast(5);
   u8g2.setFont(u8g2_font_4x6_tr);
-  u8g2.clearBuffer(); 
-  u8g2.sendBuffer(); 
+  u8g2.clearBuffer();
+  u8g2.sendBuffer();
 }
 
-void dessinInterdit(){
+void dessinInterdit() {
 
   u8g2.clearBuffer();
   u8g2.drawCircle(3, 4, 3);
@@ -203,19 +260,18 @@ void dessinInterdit(){
   u8g2.sendBuffer();
 }
 
-void dessinX(){
+void dessinX() {
   u8g2.clearBuffer();
   u8g2.drawLine(7, 0, 0, 7);
   u8g2.drawLine(0, 0, 8, 8);
   u8g2.sendBuffer();
 }
 
-void dessinWeGood(){
+void dessinWeGood() {
   u8g2.clearBuffer();
   u8g2.drawLine(4, 4, 2, 6);
   u8g2.drawLine(1, 1, 4, 4);
   u8g2.sendBuffer();
-  
 }
 
 
@@ -250,7 +306,7 @@ void analyserCommande(const String& tampon, String& commande, String& arg1, Stri
 
 
 void commande() {
- String tampon = Serial.readStringUntil('\n');
+  String tampon = Serial.readStringUntil('\n');
 
 
   // Serial.println("Réception : " + tampon);
@@ -261,31 +317,18 @@ void commande() {
   analyserCommande(tampon, commande, arg1, arg2);
 
   if (commande == "g_dist") {
-      
+    Serial.println(distance);
+    dessinWeGood();
   }
 
-  if (commande == "") {
-
+  if (commande == "cfg" && arg1 == "alm") {
+    alerte = arg2.toInt();
+    dessinWeGood();
   }
 
-  if (commande == "") {
-
+  if (commande == "cfg" && arg1 == "lim_inf") {
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
